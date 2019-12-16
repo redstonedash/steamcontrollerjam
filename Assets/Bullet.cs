7eb06d2313a2394bcede23cb9e0d40856fb2efeb
Camzoin <Camden.Cecrle@gmail.com>
2019-12-16 01:57:35 -0800

    kdixirk

---
+++ Assets/Bullet.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        GetComponent<Rigidbody>().velocity = new Vector3(50, 0, 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        GameObject.Destroy(transform.gameObject ,0);
+    }
+}
