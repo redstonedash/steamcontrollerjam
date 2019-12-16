6167b29c58fc8a5607a05224c0b867f42ca2e7d2
redstonedash <redatonedash@gmail.com>
2019-12-16 09:34:20 -0800

    done

--- Assets/timer.cs
+++ Assets/timer.cs
@@ -8,6 +8,7 @@ public class timer : MonoBehaviour
 {
     public TMP_Text timertext;
     public power  p1s, p2s;
+    float btimer = 300;
     // Start is called before the first frame update
     void Start()
     {
@@ -17,14 +18,14 @@ public class timer : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        float timer = 300 - Time.deltaTime;
-        float mins = Mathf.Floor(timer / 60);
-        float secs = Mathf.Floor(timer % 60); 
+        btimer -= Time.deltaTime;
+        float mins = Mathf.Floor(btimer / 60);
+        float secs = Mathf.Floor(btimer % 60); 
         timertext.text = mins.ToString() +":" + secs.ToString(); 
-        if (p1s.points > p2s.points  && timer < 0)
+        if (p1s.points > p2s.points  && btimer < 0)
         {
             SceneManager.LoadScene(3); 
         }
-        else if (timer < 0) { SceneManager.LoadScene(4); } 
+        else if (btimer < 0) { SceneManager.LoadScene(4); } 
     }
 }
